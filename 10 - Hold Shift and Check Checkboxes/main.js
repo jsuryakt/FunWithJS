f44b931c8b45a40614ad3e29e6af6a9bcce353b2
Jayasurya <jayasurya@digital.ai>
2023-03-30 23:11:05 +0530

    optimise: break after 2 toggles

--- 10 - Hold Shift and Check Checkboxes/main.js
+++ 10 - Hold Shift and Check Checkboxes/main.js
@@ -11,17 +11,27 @@ mailItems.forEach((mail) => {
 function handleClick(e) {
   if (e.shiftKey && this.checked) {
     if (firstChecked) {
+      let toggle = 0;
       const lastChecked = this;
       let inBetween = false;
 
-      mailItems.forEach((mail) => {
+      for (let index = 0; index < mailItems.length; index++) {
+        const mail = mailItems[index];
         if (mail === firstChecked || mail == lastChecked) {
           inBetween = !inBetween;
+          toggle++;
         }
         if (inBetween) {
           mail.checked = true;
         }
-      });
+        // to break after we check all inbetween values,
+        // don't check the elements after the second check(first/last)
+        // e.g. 10,000 elements but we only select 1-5,
+        // so break after the 5th element since we toggled 2 times, one for firstCheck and second for lastCheck
+        if (toggle == 2) {
+          break;
+        }
+      }
     }
   }
   firstChecked = this;
