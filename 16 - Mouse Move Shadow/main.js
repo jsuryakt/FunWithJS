65563d7b45017d6423fd5dabcae4135b4d411ab4
Jayasurya <jayasurya@digital.ai>
2023-04-18 01:15:37 +0530

    feat: complete mouse move

---
+++ 16 - Mouse Move Shadow/main.js
@@ -0,0 +1,19 @@
+const dance = document.querySelector("#dance");
+const moveDistance = 100;
+const danceWidth = dance.offsetWidth;
+const danceHeight = dance.offsetHeight;
+console.log(`danceWidth ${danceWidth} danceHeight ${danceHeight}`);
+// -100 to 100
+// create a function that takes moveDistance as an input and computes a value between -100 and 100 corresponding to the x and y coordinates of the mouse
+function moveBoxShadow(e) {
+    const x = e.offsetX;
+    const y = e.offsetY;
+    const xWalk = Math.round((x / danceWidth) * moveDistance - (moveDistance / 2));
+    const yWalk = Math.round((y / danceHeight) * moveDistance - (moveDistance / 2));
+    let boxShadow = `${xWalk}px ${yWalk}px 0 red`;
+    dance.style.boxShadow = boxShadow;
+    console.log(`offsetX ${x} offsetY ${y} xWalk ${xWalk} yWalk ${yWalk}`);
+}
+
+//add event listener to the dance box
+dance.addEventListener("mousemove", moveBoxShadow);
